a0a1e3c974aa310f287c7b409b6daa2eac1cf5a4
Lei Yu <leiyu@ceasia.com.cn>
2018-05-14 15:07:28 +0800

    add 3733 files

---
+++ keyboards/wm1/is31fl3733.c
@@ -0,0 +1,19 @@
+/**
+ * is31fl3733.c
+ */
+
+#include <avr/io.h>
+#include "i2c.h"
+#include "is31fl3733.h"
+
+
+void is3733_init(void)
+{}
+void is3733_set_led_state(int index, uint8_t on, uint8_t cur)
+{}
+void is3733_set_led_color(int index, uint8_t r, uint8_t g, uint8_t b)
+{}
+void is3733_set_led_pwm(int index, uint8_t pwm)
+{}
+void is3733_shutdown(void)
+{}
