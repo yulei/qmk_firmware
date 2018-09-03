3eddfe12ba6f66fd729ff48f94f2cc5aa0daf03d
Lei Yu <yuleiz@gmail.com>
2018-09-03 06:03:23 +0000

    fixed an issue on split backspace

--- keyboards/wm1/v2/wm1.h
+++ keyboards/wm1/v2/wm1.h
@@ -36,7 +36,6 @@
   {k20, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2e, k2f, xxx, xxx}, \
   {k30, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, xxx, xxx}, \
   {k40, k42, k43, k44, k48, k49, k4c, k4d, k4e, k4f, xxx, xxx, xxx, xxx, xxx, xxx} \
-	{k00, k01, k02, k03, k04, k05, k06, k07, k08}, \
 }
 
 #endif
