32d9262b70355b4be9eb5e99f644fed3300ef7ed
Lei Yu <yuleiz@gmail.com>
2019-01-04 23:14:46 +0800

    update tap setting

--- keyboards/stm60/rgb/config.h
+++ keyboards/stm60/rgb/config.h
@@ -8,7 +8,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xBEEF
-#define PRODUCT_ID      0x9999
+#define PRODUCT_ID      0xDEAD
 #define DEVICE_VER      0x0002
 #define MANUFACTURER    astro
 #define PRODUCT         stm60
@@ -18,58 +18,11 @@
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 14
 
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
-*/
-/* Note: These are not used for arm boards. They're here purely as documentation.
- * #define MATRIX_ROW_PINS { PB12, PB13, PB14, PB15, PC6, PC7, PC8, PC9}
- * #define MATRIX_COL_PINS { PB11, PB10, PB1, PB0, PC5, PC4, PA7, PA6, PA5}
- * #define UNUSED_PINS
- */
-#define TAPPING_TERM 25
+#define TAPPING_TERM 1
 #define RETRO_TAPPING
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 6
-
-/* Prevent modifiers from being stuck on after layer changes. */
-//#define PREVENT_STUCK_MODIFIERS
-
-/* backlight */
-//#define BACKLIGHT_LEVELS 1
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-//#define LOCKING_RESYNC_ENABLE
-
-/*
- * Force NKRO
- *
- * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
- * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
- * makefile for this to work.)
- *
- * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
- * until the next keyboard reset.
- *
- * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
- * fully operational during normal computer usage.
- *
- * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
- * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
- * bootmagic, NKRO mode will always be enabled until it is toggled again during a
- * power-up.
- *
- */
-//#define FORCE_NKRO
 
 /* key combination for magic key command */
 #define IS_COMMAND() ( \
@@ -94,6 +47,7 @@
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+//rgb light setting
 #define WS2812_LED_N    18
 #define RGBLED_NUM      WS2812_LED_N
 
