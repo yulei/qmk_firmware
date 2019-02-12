99e63dca0aaa3cc2641573dc0743324f52e988ea
Lei Yu <yuleiz@gmail.com>
2019-02-12 15:39:46 +0800

    add rgbv2

---
+++ keyboards/stm60/rgbv2/config.h
@@ -0,0 +1,67 @@
+/**
+ * config.h
+ *
+ */
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xDEAD
+#define PRODUCT_ID      0xFEED
+#define DEVICE_VER      0x6002
+#define MANUFACTURER    astro
+#define PRODUCT         60rgbv2
+#define DESCRIPTION     60% rgb keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+/* key matrix pins */
+#define MATRIX_ROW_PINS { F0, F1, B0, B3, B4 }
+#define MATRIX_COL_PINS { B6, C6, C7, F7, F6, F5, F4, E6, B1, B2, B7, D2, D3, D5, B5}
+#define UNUSED_PINS
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
+
+//rgb light setting
+#define RGBLED_NUM      18
+#define RGB_DI_PIN      D7
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+
+//rgb matrix setting
+#define DRIVER_ADDR_1 0b1110100
+#define DRIVER_ADDR_2 0b1110111
+#define DRIVER_COUNT 2
+#define DRIVER_1_LED_TOTAL 36
+#define DRIVER_2_LED_TOTAL 36
+#define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL
+
+// tapping setting
+#define TAPPING_TERM    200
+#define RETRO_TAPPING
+#define PERMISSIVE_HOLD
