0de5f8d4a2aed144740f7c02bb17c5f638ad3261
Lei Yu <yuleiz@gmail.com>
2019-05-26 00:11:18 +0800

    initial version for the update matrix format

--- keyboards/stm60/rb40/rules.mk
+++ keyboards/stm60/rb40/rules.mk
@@ -72,5 +72,5 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-RGBLIGHT_ENABLE = yes       # Use RGB bottom light
-#RGB_MATRIX_ENABLE = yes     # Use RGB matrix
+#RGBLIGHT_ENABLE = yes       # Use RGB bottom light
+RGB_MATRIX_ENABLE = IS31FL3737 # Use RGB matrix
