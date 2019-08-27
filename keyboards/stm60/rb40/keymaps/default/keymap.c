124ea60c0b5198ee461bc11da6ab550129bbfc19
Lei Yu <yuleiz@gmail.com>
2019-08-27 12:15:08 +0800

    update keymap and info.json for rb40

--- keyboards/stm60/rb40/keymaps/default/keymap.c
+++ keyboards/stm60/rb40/keymaps/default/keymap.c
@@ -13,8 +13,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------'
      */
     [0] = LAYOUT_default(
-      KC_ESC,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,   KC_BSPC,
-      KC_TAB,     RGB_TOG,   RGB_MOD,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,     KC_ENT,
-      KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_UP,   KC_RSFT,
-      KC_LCTL,  KC_LGUI,  KC_LALT,        MO(1),          KC_SPC,         KC_LEFT, KC_DOWN, KC_RIGHT)
+        KC_ESC,     KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U, KC_I,    KC_O,    KC_P,   KC_BSPC,
+        KC_TAB,     KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J, KC_K,    KC_L,    KC_ENT,
+        KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM, KC_UP,   KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT,           MO(1),        KC_SPC,         KC_LEFT, KC_DOWN, KC_RIGHT),
+    [1] = LAYOUT_default(
+        _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,     KC_0, KC_DEL,
+        _______, RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______,          _______,          _______, _______, _______)
 };
--- keyboards/stm60/rb40/rules.mk
+++ keyboards/stm60/rb40/rules.mk
@@ -65,12 +65,12 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes            # USB Nkey Rollover
+NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 UNICODE_ENABLE = no         # Unicode
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 #RGBLIGHT_ENABLE = yes       # Use RGB bottom light
-RGB_MATRIX_ENABLE = IS31FL3737 # Use RGB matrix
-BLUETOOTH = AdafruitBLE
+#RGB_MATRIX_ENABLE = IS31FL3737 # Use RGB matrix
+#BLUETOOTH = AdafruitBLE
