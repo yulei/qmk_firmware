e43b54e09e49431d2e16730478d610d2abb2009d
Lei Yu <yuleiz@gmail.com>
2019-12-30 12:51:06 +0800

    fixed dma channel

--- keyboards/stm60/rgb/config.h
+++ keyboards/stm60/rgb/config.h
@@ -54,7 +54,7 @@
 #define WS2812_TIM_CH 1
 #define PORT_WS2812 GPIOA
 #define PIN_WS2812 7
-#define WS2812_DMA_STREAM STM32_DMA1_STREAM5  // DMA stream for TIMx_UP (look up in reference manual under DMA Channel selection)
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM2  // DMA stream for TIMx_UP (look up in reference manual under DMA Channel selection)
 #define WS2812_DMA_CHANNEL 5                  // DMA channel for TIMx_UP
 
 #define RGBLIGHT_ANIMATIONS
