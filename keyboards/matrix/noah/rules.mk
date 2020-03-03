66f8917fc4eef04fe2c8d0bfe75b9bab7d06493e
Lei Yu <yuleiz@gmail.com>
2020-03-03 12:25:46 +0800

    fixed rgb led position issue

--- keyboards/matrix/noah/rules.mk
+++ keyboards/matrix/noah/rules.mk
@@ -37,7 +37,7 @@ OPT_DEFS =
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes	    # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
