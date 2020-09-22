4ebf17f5c1d64e22441695e01ea46204f3da264a
Lei Yu <yuleiz@gmail.com>
2020-09-22 10:28:59 +0800

    fixed rgb matrix issue

--- keyboards/inett_studio/sqx/universal/universal.c
+++ keyboards/inett_studio/sqx/universal/universal.c
@@ -160,7 +160,7 @@ led_config_t g_led_config = {
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch(keycode) {
-#if defined(RGB_MATRIX_DISABLE_KEYCODES)
+//#if defined(RGB_MATRIX_DISABLE_KEYCODES)
         case RGB_MATRIX_TOGGLE: // toggle rgb matrix
             rgb_matrix_toggle();
             return false;
@@ -185,7 +185,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
         case RGB_MATRIX_DEC_VAL:
             rgb_matrix_decrease_val();
             return false;
-#endif
+//#endif
         default:
             break;
         }
